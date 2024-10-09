89dd2af64367299438d6592f69f6d375dd4559f0
joachim <108612023+bangjopl@users.noreply.github.com>
2024-10-09 16:09:40 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Update sources.list to use archive.debian.org
+sed -i 's/deb.debian.org/archive.debian.org/g' /etc/apt/sources.list
+sed -i 's|security.debian.org|archive.debian.org|g' /etc/apt/sources.list
+sed -i '/stretch-updates/d' /etc/apt/sources.list
+
+# Update and upgrade the system
+apt update -y
+apt upgrade -y
+apt install traceroute
+apt install openssh-client
+
+# chmod +x update_sources.sh
+# sudo ./update_sources.sh
